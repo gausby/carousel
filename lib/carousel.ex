e0fd126d611b622005e2196ee7d1e7b5e7197ea2
Martin Gausby <martin@gausby.dk>
2016-06-17 00:01:46 +0200

    Add hard_stop configuration option
    - Track the position in the carousel when advancing
    
    - Track the length of the carousel when adding elements
    
    - Updated the documentation
    
    - Added a `hard_stop` option that changes the semantics when the end of
      the queue has been reached. If `true` it will stop taking elements
      from the queue when reaching the last element; if `false` it will wrap
      around and continue taking elements until the number of requested
      elements has been fulfilled
    
    - Major restructure of the unit tests

--- lib/carousel.ex
+++ lib/carousel.ex
@@ -4,29 +4,54 @@ defmodule Carousel do
   is implemented with regular Erlang queues, an element taken out is
   inserted to the end of the queue.
   """
-  defstruct queue: {[], []}
+  defstruct queue: {[], []}, position: 0, length: 0, hard_stop: false
 
   @doc """
-  Create a new carousel.
+  Creates a new carousel.
 
   A carousel can be initialized with items by passing in a list of
   elements.
+
+  One configuration option, `hard_stop`, is currently available. It
+  changes the behaviour when taking items out of the carousel. If set
+  to `false` (default) it will wrap around to the start of the queue
+  when the end has been reached, and continue taking items out until
+  it hits the number of requested items.
+
+      iex> Carousel.new([:foo, :bar, :baz])
+      ...> |> Carousel.take(4)
+      ...> |> elem(0)
+      [:foo, :bar, :baz, :foo]
+
+  With hard stop set to `true` it will stop at the last item, even if
+  asked for more items than remaining.
+
+      iex> Carousel.new([:foo, :bar, :baz], hard_stop: true)
+      ...> |> Carousel.take(4)
+      ...> |> elem(0)
+      [:foo, :bar, :baz]
+
   """
-  def new(items \\ [], _opts \\ []) do
-    %Carousel{queue: :queue.from_list(items)}
+  def new(items \\ [], opts \\ []) do
+    %Carousel{queue: :queue.from_list(items),
+              length: length(items),
+              hard_stop: Keyword.get(opts, :hard_stop, :false)}
   end
 
   @doc """
   Insert a new item to the carousel. It will get inserted in the back
-  so it will get returned when the carousel has made a cycle.
+  of the queue relative to the current position.
   """
-  def insert(%Carousel{queue: queue} = carousel, item) do
-    %{carousel|queue: :queue.snoc(queue, item)}
+  def insert(%Carousel{queue: queue, length: len} = carousel, item) do
+    %{carousel|queue: :queue.snoc(queue, item),
+               length: len + 1}
   end
 
   @doc """
-  Take `n` elements from the carousel. If `n` is bigger than the queue
-  it will repeat items.
+  Take `n` elements from the carousel.
+
+  See the documentation for `Carousel.new/2` for details on the
+  semantics when the last element is reached.
   """
   def take(carousel, n) when is_number(n) and n >= 0 do
     do_cycle(carousel, n)
@@ -43,7 +68,14 @@ defmodule Carousel do
   defp do_cycle(%Carousel{queue: queue} = carousel, n, acc) do
     {{:value, item}, temp_queue} = :queue.out(queue)
     new_queue = :queue.snoc(temp_queue, item)
+    new_position = rem(carousel.position + 1, carousel.length)
 
-    do_cycle(%{carousel|queue: new_queue}, n - 1, [item|acc])
+    # if `hard_stop` is `true` it should not be able to wrap around in
+    # the same cycle.
+    next =
+      unless carousel.hard_stop && new_position == 0, do: n - 1, else: 0
+
+    do_cycle(%{carousel|queue: new_queue,
+                        position: new_position}, next, [item|acc])
   end
 end
--- test/carousel_test.exs
+++ test/carousel_test.exs
@@ -2,49 +2,74 @@ defmodule CarouselTest do
   use ExUnit.Case
   doctest Carousel
 
-  test "should be able to insert into the carousel queue" do
-    carousel = Carousel.new
-    assert (Carousel.insert(carousel, :foo)).queue == {[:foo], []}
+  describe "initialization" do
+    test "with no initial data" do
+      carousel = Carousel.new
+
+      assert carousel.queue == {[], []}
+      assert carousel.length == 0
+    end
+
+    test "with a list of data" do
+      carousel = Carousel.new([:foo, :bar, :baz])
+
+      assert carousel.queue == {[:baz], [:foo, :bar]}
+      assert carousel.length == 3
+    end
   end
 
-  test "initialization with no initial data" do
-    carousel = Carousel.new
-    assert carousel.queue == {[], []}
+  describe "inserting data" do
+    test "should be able to insert into the carousel queue" do
+      carousel =
+        Carousel.new
+        |> Carousel.insert(:foo)
+
+      assert carousel.queue == {[:foo], []}
+      assert carousel.length == 1
+    end
+
+    test "data should come out the same order it went in" do
+      data = [:foo, :bar, :baz, :quun]
+      queue =
+        Enum.reduce(data, Carousel.new, fn item, acc ->
+          Carousel.insert(acc, item)
+        end)
+
+      assert {^data, _} = Carousel.take(queue, 4)
+    end
+
+    test "data should come out the same order it went in (with initial data)" do
+      data = [:foo, :bar, :baz, :quun]
+      queue =
+        Enum.reduce(Enum.drop(data, 2), Carousel.new(Enum.take(data, 2)), fn item, acc ->
+          Carousel.insert(acc, item)
+        end)
+
+      assert {^data, _} = Carousel.take(queue, 4)
+    end
   end
 
-  test "initialization with a list of data" do
-    carousel = Carousel.new([:foo, :bar, :baz])
-    assert carousel.queue == {[:baz], [:foo, :bar]}
-  end
+  describe "cycling / receiving items" do
+    test "return an empty result list when cycling a carousel without data" do
+      assert {[], %Carousel{}} = Carousel.take(Carousel.new(), 4)
+    end
 
-  test "inserted data should come out the same order it went in" do
-    data = [:foo, :bar, :baz, :quun]
-    queue =
-      Enum.reduce(data, Carousel.new, fn item, acc ->
-        Carousel.insert(acc, item)
-      end)
+    test "advance position when taking items from the carousel" do
+      carousel = Carousel.new([:foo, :bar, :baz])
 
-    assert {^data, _} = Carousel.take(queue, 4)
-  end
+      assert {_, %Carousel{position: 1}} = Carousel.take(carousel, 1)
+    end
 
-  test "inserted data should come out the same order it went in (with initial data)" do
-    data = [:foo, :bar, :baz, :quun]
-    queue =
-      Enum.reduce(Enum.drop(data, 2), Carousel.new(Enum.take(data, 2)), fn item, acc ->
-        Carousel.insert(acc, item)
-      end)
+    test "cycling items (no hard stop - wrap around)" do
+      carousel = Carousel.new([:foo, :bar, :baz])
 
-    assert {^data, _} = Carousel.take(queue, 4)
-  end
+      assert {[:foo, :bar, :baz, :foo], %Carousel{position: 1}} = Carousel.take(carousel, 4)
+    end
 
-  test "should cycle the items in the carousel" do
-    carousel =
-      Carousel.new([:foo, :bar, :baz])
+    test "cycling items (with hard stop - no wrap around)" do
+      carousel = Carousel.new([:foo, :bar, :baz], hard_stop: true)
 
-    assert {[:foo, :bar, :baz, :foo], %Carousel{}} = Carousel.take(carousel, 4)
-  end
-
-  test "should return an empty result list when cycling a carousel without data" do
-    assert {[], %Carousel{}} = Carousel.take(Carousel.new(), 4)
+      assert {[:foo, :bar, :baz], %Carousel{position: 0}} = Carousel.take(carousel, 4)
+    end
   end
 end
